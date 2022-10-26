134c10621f14992dda887fb6fbe1bd9b040f0494
pc20211139 <115052276+pc20211139@users.noreply.github.com>
2022-10-26 09:21:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,16 @@ input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 5; index++) {
         for (let index = 0; index < 5; index++) {
             led.plot(xvalue, yvalue)
-            basic.pause(500)
+            basic.pause(200)
             xvalue += 1
+            led.plot(xvalue, 0)
+        }
+        yvalue += 1
+        xvalue = 4
+        for (let index = 0; index < 5; index++) {
+            led.plot(xvalue, yvalue)
+            basic.pause(200)
+            xvalue += -1
             led.plot(xvalue, 0)
         }
         yvalue += 1
