a5082203aea85aacbe482bd45fc37a953b819a6d
pc20211139 <115052276+pc20211139@users.noreply.github.com>
2022-10-26 08:57:56 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(xvalue, yvalue)
+            basic.pause(500)
+            xvalue += 1
+            led.plot(xvalue, 0)
+        }
+        yvalue += 1
+        xvalue = 0
+    }
+})
+let yvalue = 0
+let xvalue = 0
+xvalue = 0
+yvalue = 0
+basic.forever(function () {
+	
+})
